d4c79501a77aa761cac2aa950a4616e4f76e09dd
Karl Mayer <khmayer01@gmail.com>
2026-05-07 13:39:36 -0600

    Update TQ_cycle_benchmarking.py
    Added order_in_zones to TQ cycle benchmarking

--- TQ_cycle_benchmarking.py
+++ TQ_cycle_benchmarking.py
@@ -26,6 +26,8 @@ from analysis_tools import marginalize_hists, get_postselection_rates, postselec
 import bootstrap as bs
 from randomized_compiling import rand_comp_rzz
 
+from qtm_platform.ops import order_in_zones
+
 
 Paulis = ['IX','IY','IZ','XI','XX','XY','XZ','YI','YX','YY','YZ','ZI','ZX','ZY','ZZ']
 
@@ -45,7 +47,8 @@ class CB_Experiment(Experiment):
         self.seq_lengths = seq_lengths
         self.setting_labels = ('seq_len', 'init_state')
         
-        self.options['barriers'] = True
+        self.options['barriers'] = False
+        self.options['order_in_zones'] = kwargs.get('order_in_zones', True)
         self.options['experiment_size'] = kwargs.get('experiment_size', 'small')
         self.options['init_seed'] = kwargs.get('init_seed', 12345)
         self.options['parallel_input_states'] = kwargs.get('parallel_input_states', False)
@@ -102,6 +105,7 @@ class CB_Experiment(Experiment):
         qubits = self.qubits
         barriers = self.options['barriers']
         meas_leak = self.options['measure_leaked']
+        order_qubits = self.options['order_in_zones']
         n_qubits = self.n_qubits
         init_seed = self.options['init_seed']
         
@@ -155,6 +159,8 @@ class CB_Experiment(Experiment):
             rng = RNG(comptime(init_seed) + get_current_shot())
             final_Xs = array(rng.random_int_bounded(2) for _ in range(comptime(n_qubits)))
             q = array(qubit() for _ in range(comptime(n_qubits)))
+            if comptime(order_qubits):
+                order_in_zones(q)
             
             for gate_id, q_id in comptime(init_commands):
                 if gate_id == 1:
